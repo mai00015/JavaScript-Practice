8f6080b53f3854fe9ecba4f20b41979c847c98a2
Kha Mai <kha.maitran@gmail.com>
2020-08-31 23:30:42 -0400

    Update app.js

--- CSS-Variables/app.js
+++ CSS-Variables/app.js
@@ -6,6 +6,5 @@
       const suffix = this.dataset.sizing || '';
       document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
     }
-
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));

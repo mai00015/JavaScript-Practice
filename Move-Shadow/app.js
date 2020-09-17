4130c9b27d8f3cae61c8c860d941f6ceed260feb
Kha Mai <kha.maitran@gmail.com>
2020-09-16 23:19:29 -0400

    Update app.js

--- Move-Shadow/app.js
+++ Move-Shadow/app.js
@@ -1,25 +1,24 @@
 const hero = document.querySelector('.hero');
-  const text = hero.querySelector('h1');
-  const walk = 460; // 100px
+const text = hero.querySelector('h1');
+const walk = 460; // 100px
 
-  function shadow(e){
-    const {offsetWidth: width, offsetHeight: height} = hero;
-    let {offsetX: x, offsetY: y} = e;
+function shadow(e){
+  const {offsetWidth: width, offsetHeight: height} = hero;
+  let {offsetX: x, offsetY: y} = e;
 
-    if(this !== e.target){
-      x = x + e.target.offsetLeft;
-      y = y + e.target.offsetTop;
-    }
-
-    const xWalk = Math.round((x / width * walk) - (walk /2));
-    const yWalk = Math.round((y / height * walk) - (walk /2));
-
-    text.style.textShadow = `
-      ${xWalk}px ${yWalk}px 0 purple,
-      ${xWalk * -1}px ${yWalk}px 0 orange,
-      ${yWalk}px ${xWalk * -1}px 0 yellow,
-      ${yWalk * -1}px ${xWalk}px 0 rgb(95, 85, 189)
-    `;
+  if(this !== e.target){
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
   }
 
+  const xWalk = Math.round((x / width * walk) - (walk /2));
+  const yWalk = Math.round((y / height * walk) - (walk /2));
+
+  text.style.textShadow = `
+    ${xWalk}px ${yWalk}px 0 purple,
+    ${xWalk * -1}px ${yWalk}px 0 orange,
+    ${yWalk}px ${xWalk * -1}px 0 yellow,
+    ${yWalk * -1}px ${xWalk}px 0 rgb(95, 85, 189)
+  `;
+}
 hero.addEventListener('mousemove', shadow);
